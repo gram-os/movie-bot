4318f7fa03965b1b642af7244e55aebcdf199bf3
gramos0211 <45494272+gramos0211@users.noreply.github.com>
2022-02-23 02:44:47 -0500

    reformat code, fix tests, ignore exception test for fetching movie titles

--- src/imsdb_fetch.py
+++ src/imsdb_fetch.py
@@ -6,14 +6,17 @@ BASE_URL = "https://imsdb.com/"
 ALL_SCRIPTS = "all-scripts.html"
 SCRIPT_BASE = "scripts/"
 
+
 class ScriptNotFoundException(Exception):
     """Exception thrown when script could not be found"""
     pass
 
+
 class MoviesNotFoundException(Exception):
     """Exception thrown when movies could not be found"""
     pass
-    
+
+
 def fetchMovieTitles():
     page = requests.get(BASE_URL + ALL_SCRIPTS)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -22,25 +25,27 @@ def fetchMovieTitles():
 
     movieTitles = []
     for p in tableMovieEntries:
-        movieTitles.append(p.find('a', href = True).contents[0])
-    
+        movieTitles.append(p.find('a', href=True).contents[0])
+
     if len(movieTitles) <= 0:
         raise MoviesNotFoundException
-    
+
     return movieTitles
+
 
 def pickRandomMovie():
     movies = fetchMovieTitles()
     return random.choice(movies)
 
-def getMovieScript(movieTitle = pickRandomMovie()):
-    parsedMovieTitle = movieTitle.replace(' ','-')
+
+def getMovieScript(movieTitle=pickRandomMovie()):
+    parsedMovieTitle = movieTitle.replace(' ', '-')
     page = requests.get(BASE_URL + SCRIPT_BASE + parsedMovieTitle + '.html')
-    
+
     soup = BeautifulSoup(page.content, "html.parser")
-    script = soup.find(class_ = "scrtext").find('pre').text
+    script = soup.find(class_="scrtext").find('pre').text
 
     if len(script) <= 0:
         raise ScriptNotFoundException
-    
+
     return script
--- test/test_imsdb.py
+++ test/test_imsdb.py
@@ -1,36 +1,37 @@
 import unittest
 from src import imsdb_fetch
 
-class TestMovieFetchingcd(unittest.TestCase):
-    def fetchMovieTitlesSucceeds(self):
+class TestMovieFetching(unittest.TestCase):
+    def testFetchMovieTitlesSucceeds(self):
         """Test that we can fetch movies successfully"""
-        
+
         movieTitles = imsdb_fetch.fetchMovieTitles()
         self.assertIsNotNone(movieTitles)
-    
-    def fetchMovieTitlesFailsRaisesException(self):
+
+    @unittest.skip("need to mock to ensure we can throw exception")
+    def testFetchMovieTitlesFailsRaisesException(self):
         """Tests that when no movie titles are returned, we raise an exception"""
-        
-        movieTitles = imsdb_fetch.fetchMovieTitles()
-        self.assertRaises(imsdb_fetch.MoviesNotFoundException)
-    
-    def fetchRandomMovieTitleSucceeds(self):
+
+        with self.assertRaises(imsdb_fetch.MoviesNotFoundException):
+            imsdb_fetch.fetchMovieTitles()
+
+    def testFetchRandomMovieTitleSucceeds(self):
         """Ensure we get a single title"""
-        
+
         movieTitle = imsdb_fetch.pickRandomMovie()
         self.assertIsNotNone(movieTitle)
-    
-    def fetchMovieScriptSucceeds(self):
+
+    def testFetchMovieScriptSucceeds(self):
         """Ensure we get script successfully"""
-        
+
         script = imsdb_fetch.getMovieScript()
         self.assertIsNotNone(script)
-        
-    def fetchMovieScriptFails(self):
+
+    def testFetchMovieScriptFails(self):
         """Ensure we raise exception"""
-        
-        script = imsdb_fetch.getMovieScript("fdafkldsafj")
-        self.assertRaises(imsdb_fetch.ScriptNotFoundException)
+
+        with self.assertRaises(imsdb_fetch.ScriptNotFoundException):
+            imsdb_fetch.getMovieScript("fdafkldsafj")
 
 if __name__ == '__main__':
     unittest.main()
